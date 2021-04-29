979a78651e43cf5bdc5234e787a60247aa66124e
Joseph Destrampe <gaiagreenworks@gmail.com>
2021-04-28 18:58:17 -0700

    Install Express and Configure Basic Routes

---
+++ dev/api.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/blockchain', (req, res) => {
+
+});
+
+app.post('/transaction', (req, res) => {
+
+});
+
+app.get('/mine', (req, res) => {
+
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000...');
+});
