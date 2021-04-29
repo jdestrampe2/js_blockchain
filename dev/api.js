bff823d90d5282e11deed7831f1eb113a191ccd0
Joseph Destrampe <gaiagreenworks@gmail.com>
2021-04-28 19:53:03 -0700

    Basic POST Route Buildout and Test with Postman

--- dev/api.js
+++ dev/api.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/blockchain', (req, res) => {
 
 });
 
 app.post('/transaction', (req, res) => {
-
+  console.log(req.body);
+  res.send(`The amount of the transaction is ${req.body.amount} gooncoin.`);
 });
 
 app.get('/mine', (req, res) => {
