f6187abd1c8c07634573400b7f6f175bdbdc8eca
Joseph Destrampe <gaiagreenworks@gmail.com>
2021-04-28 18:39:08 -0700

    Implement Genesis Block Method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,7 +3,8 @@ const sha256 = require('sha256');
 function Blockchain() {
 	this.chain = [];
 	this.pendingTransactions = [];
-}
+  this.createNewBlock(100, '0', '0');
+};
 
 Blockchain.prototype.createNewBlock = function (
 	nonce,
@@ -43,20 +44,28 @@ Blockchain.prototype.createNewTransaction = function (
 	return this.getLastBlock()['index'] + 1;
 };
 
-Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce) {
-  const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
-  const hash = sha256(dataAsString);
-  return hash;
+Blockchain.prototype.hashBlock = function (
+	previousBlockHash,
+	currentBlockData,
+	nonce
+) {
+	const dataAsString =
+		previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+	const hash = sha256(dataAsString);
+	return hash;
 };
 
-Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
-  let nonce = 0;
-  let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-  while (hash.substring(0, 4) !== '0000') {
-    nonce++;
-    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-  }
-  return nonce;
+Blockchain.prototype.proofOfWork = function (
+	previousBlockHash,
+	currentBlockData
+) {
+	let nonce = 0;
+	let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+	while (hash.substring(0, 4) !== '0000') {
+		nonce++;
+		hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+	}
+	return nonce;
 };
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,23 +1,5 @@
 const Blockchain = require('./blockchain');
 const gooncoin = new Blockchain();
 
-const previousBlockHash = '1234wasd';
-const currentBlockData = [
-  {
-    amount: 100,
-    sender: 'asdfasdewqwetr',
-    recipient: 'oiutrkjasnfdks'
-  },
-  {
-    amount: 150,
-    sender: '1asdfasdewqwetr',
-    recipient: '1oiutrkjasnfdks'
-  },
-  {
-    amount: 10,
-    sender: '2asdfasdewqwetr',
-    recipient: '2oiutrkjasnfdks'
-  },
-];
+console.log(gooncoin);
 
-console.log(gooncoin.hashBlock(previousBlockHash, currentBlockData, 7681));
